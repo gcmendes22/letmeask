4ae779aaea06ef063f74277e5fac705007568438
gcmendes22 <gcmedia.official@gmail.com>
2021-06-23 17:51:57 +0100

    creating auth page and create-room page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import IllustrationImg from '../assets/images/illustration.svg';
+import LogoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/global.scss';
+import '../styles/auth.scss';
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={IllustrationImg} alt="Background" />
+        <strong>create live Q&amp;A rooms</strong>
+        <p>Clarify the doubts of your audience in real time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={LogoImg} alt="Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Google Logo" />
+            Create your room with Google
+          </button>
+          <div className="separator">Or enter a room</div>
+          <form>
+            <input type="text" placeholder="Enter the room code"/>
+            <Button type="submit">
+              Enter the room
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,33 @@
+import IllustrationImg from '../assets/images/illustration.svg';
+import LogoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/global.scss';
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={IllustrationImg} alt="Background" />
+        <strong>create live Q&amp;A rooms</strong>
+        <p>Clarify the doubts of your audience in real time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={LogoImg} alt="Letmeask" />
+          <h2>Create new room</h2>
+          <form>
+            <input type="text" placeholder="Room name"/>
+            <Button type="submit">
+              Enter the room
+            </Button>
+          </form>
+          <p>Want to join an existing room? <a href="#">Click here!</a></p>
+        </div>
+      </main>
+    </div>
+  );
+}
